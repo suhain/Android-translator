b46264d1d138b8b20bb8f537f787e956b58601d7
Denis Mekhanikov <Megabyte777@bigmir.net>
2012-10-16 22:27:51 +0400

    Edited ImageSearcher

--- src/ru/ifmo/mobdev/android_translator/ImageSearcher.java
+++ src/ru/ifmo/mobdev/android_translator/ImageSearcher.java
@@ -7,64 +7,59 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class ImageSearcher {
-	private static final String queryPrefix = "https://www.google.ru/search?tbm=isch&q=";
+	private static final String queryPrefix = 
+			"https://www.google.ru/search?tbm=isch&tbs=isz:m&q=";
 	private static final String begFlag = "imgurl=";
 	private static final String endFlag = "&amp;";
 
-	private static String query = null; // defined here to let execQuery() work in a new thread
-	private static volatile StringBuilder sourceString = null; // same
-
-	public static synchronized String[] searchImages(String queryWord) {
-		query = queryPrefix;
+	private String sourceString = null;
+	private int cur = 0;
+	
+	public ImageSearcher(String queryWord) {
+		init(queryWord);
+	}
+	
+	private void init(String queryWord) {
+		String query = queryPrefix;
 		for (int i = 0; i != queryWord.length(); i++) {
 			query += (queryWord.charAt(i) == ' ' ? '+' : queryWord.charAt(i));
 		}
-		Thread thread = new Thread(new Runnable() {
-			public void run() {
-				execQuery();
-			}
-		});
-		thread.start();
-		try {
-			thread.join();
-		} catch (InterruptedException e) {
-			return null;
-		}
-		if (sourceString == null) { // must be changed in another thread
-			return null;
-		}
-
-		String[] links = new String[10];
-		int cur = sourceString.indexOf(begFlag);
-		for (int i = 0; cur != -1 && i != 10; i++) {
-			cur += begFlag.length();
-			links[i] = sourceString.substring(cur,
-					sourceString.indexOf(endFlag, cur));
-			cur = sourceString.indexOf(begFlag, cur);
-		}
-		return links;
-	}
-
-	private static void execQuery() {
 		URL url;
 		try {
 			url = new URL(query);
 		} catch (MalformedURLException e) {
-			sourceString = null;
 			return;
 		}
+		StringBuilder source = null;
 		try {
 			BufferedReader sourceReader = new BufferedReader(
 					new InputStreamReader(url.openStream()));
-			sourceString = new StringBuilder();
+			source = new StringBuilder();
 			String line = sourceReader.readLine();
 			while (line != null) {
-				sourceString.append(line);
+				source.append(line);
 				line = sourceReader.readLine();
 			}
 		} catch (IOException e) {
-			sourceString = null;
 			return;
+		}
+		sourceString = source.toString();
+	}
+	
+	public String nextUrl() {
+		if (sourceString == null) {
+			return null;
+		}
+		cur = sourceString.indexOf(begFlag, cur);
+		if (cur == -1) {
+			return null;
+		}
+		cur += begFlag.length();
+		int end = sourceString.indexOf(endFlag, cur);
+		if (end != -1) {
+			return sourceString.substring(cur, end);
+		} else {
+			return null;
 		}
 	}
 }
--- src/ru/ifmo/mobdev/android_translator/MainActivity.java
+++ src/ru/ifmo/mobdev/android_translator/MainActivity.java
@@ -1,23 +1,29 @@
 package ru.ifmo.mobdev.android_translator;
 
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.util.Log;
 import android.widget.EditText;;
 
 public class MainActivity extends Activity {
 
-    @Override
+    @SuppressLint("NewApi")
+	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String[] result = ImageSearcher.searchImages("weasel");
+        
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+		StrictMode.setThreadPolicy(policy);
+		
         EditText editText = (EditText) findViewById(R.id.editText1);
-        if (result != null) {
-	        for (int i = 0; i != 10 && result[i] != null; ++i) {
-	        	editText.append(result[i] + "\n\n");
-	        }
-        } else {
-        	editText.setText("error!");
+        ImageSearcher imgSearcher = new ImageSearcher("weasel"); 
+        for (int i = 0; i != 10; ++i) {
+        	String url = imgSearcher.nextUrl();
+        	editText.append(url + "\n\n");
+        	Log.i("Link", url);
         }
     }
 }
